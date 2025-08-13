5c7c8b1cd29743985f808674e90e5253a3ccbc69
nikamushi <autoruns18@gmail.com>
2025-08-13 18:15:24 +0800

    fix index.js and add frontend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Option 2:
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
